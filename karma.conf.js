7921eb6ce6c5bed93e62edfc6571cd196a056dff
Dawid Paweł Sowa <dawidpawelsowa@gmail.com>
2021-11-27 01:02:15 +0100

    feat: Add release action, update readme

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/owl-app'),
+      dir: require('path').join(__dirname, './coverage/in-excel-report'),
       subdir: '.',
       reporters: [
         { type: 'html' },
