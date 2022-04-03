1d3e60d954d34edaac7456382e17b0643422c363
Dawid Sowa <dawidpawelsowa@gmail.com>
2022-04-03 02:00:01 +0200

    feat: add GH link, bump deps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
+import { environment } from './../environments/environment';
 import {
-  AfterViewInit,
   Component,
-  OnDestroy,
-  OnInit,
-  ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
 
+declare const gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    this.addGAScript();
+  }
+
+  addGAScript() {
+    let gtagScript: HTMLScriptElement = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + environment.googleAnalyticsId;
+    document.head.prepend(gtagScript);
+    gtag('config', environment.googleAnalyticsId, { send_page_view: false });
+}
+}
